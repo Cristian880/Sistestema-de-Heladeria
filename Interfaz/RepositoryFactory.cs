c5623c974c78cdf279dd0a036e7c6b03552f4d62
Cristian Luna <cristianlunarosario08@gmail.com>
2025-04-22 14:07:34 -0400

    Restaurar archivos importantes del proyecto

---
+++ Interfaz/RepositoryFactory.cs
@@ -0,0 +1,30 @@
+﻿using CapaDatos.Interfaz;
+using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+namespace Sistema.Interfaz
+{
+    public static class RepositoryFactory
+    {
+        public static IGenericRepository<T> CreateRepository<T>(string connectionString) where T : class, new()
+        {
+            if (typeof(T) == typeof(Productos))
+                return (IGenericRepository<T>)new ProductoRepository(connectionString);
+
+            if (typeof(T) == typeof(Usuarios))
+                return (IGenericRepository<T>)new UsuarioRepository(connectionString);
+
+            if (typeof(T) == typeof(Clientes))
+                return (IGenericRepository<T>)new ClienteRepository(connectionString);
+
+            // Otros tipos...
+
+            throw new ArgumentException($"No hay repositorio definido para el tipo {typeof(T).Name}");
+        }
+    }
+}
+*/
---
+++ UsuarioDA.cs
@@ -0,0 +1,54 @@
+﻿using CapaEntidades;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class UsuarioDA : GenericRepository<Usuarios>
+    {
+        public UsuarioDA() : base(ConexionDA.CadenaConexion) { }
+
+        public Usuarios AutenticarUsuario(string userMail, string password)
+        {
+            using (SqlConnection conexion = new SqlConnection(ConexionDA.CadenaConexion))
+            {
+                try
+                {
+                    conexion.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("AutenticarUsuario", conexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@GmailUsuario", userMail);
+                        cmd.Parameters.AddWithValue("@Clave", password);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Usuarios usuario = new Usuarios();
+
+                                // Mapear propiedades
+                                usuario.IdUsuario = Convert.ToInt32(reader["idUsuario"]);
+                                usuario.Nombre = reader["Nombre"].ToString();
+                                usuario.Apellido = reader["Apellido"].ToString();
+                                usuario.GmailUsuario = reader["GmailUsuario"].ToString();
+                                usuario.Rol = reader["Rol"].ToString();
+                                usuario.Estado = Convert.ToBoolean(reader["Estado"]);
+
+                                return usuario;
+                            }
+                        }
+                    }
+
+                    return null; // Usuario no encontrado
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error al autenticar usuario: " + ex.Message);
+                }
+            }
+        }
+    }
+}
